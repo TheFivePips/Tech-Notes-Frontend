d07ecf89f34e469958aa9ab7978f2770b98bd07f
Sam Gardner <samgardner20@gmail.com>
2023-01-23 11:36:03 -0500

    added in basic file structure and set up basic componenets for login, welcome, notes, and styling

---
+++ src/components/DashLayout.js
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import DashHeader from './DashHeader';
+import DashFooter from './DashFooter';
+
+const DashLayout = () => {
+    return (
+        <>
+            <div className='dash-container'>
+                <DashHeader />
+                <Outlet />
+                <DashFooter />
+            </div>
+        </>
+    );
+}
+
+export default DashLayout;
